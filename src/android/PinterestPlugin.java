72550783fbc90e2a3b355e8cbe036f6b0811cffb
Ryan Sundberg <ryan.sundberg@gmail.com>
2016-12-27 21:27:38 -0800

    Do not require (lacklustre) pdk for android, use native Intent

--- src/android/PinterestPlugin.java
+++ src/android/PinterestPlugin.java
@@ -6,12 +6,11 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CordovaPlugin;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
-
-import com.pinterest.pinit.PinIt;
-import com.pinterest.pinit.PinItButton;
-import com.pinterest.pinit.PinItListener;
 
 public class PinterestPlugin extends CordovaPlugin {
   public static final String TAG = "PinterestPlugin";
@@ -30,69 +29,51 @@ public class PinterestPlugin extends CordovaPlugin {
   @Override
   public boolean execute(String action, CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
     boolean result = false;
-    final String sourceUrl, imageUrl, description;
-    final PinItListener _listener = new PinItListener() {
-      @Override
-      public void onStart() {
-        super.onStart();
-        Log.i(TAG, "PinItListener.onStart");
-      }
-
-      @Override
-      public void onComplete(boolean completed) {
-        super.onComplete(completed);
-        Log.i(TAG, "PinItListener.onComplete");
-        if (completed) {
-          callbackContext.success();
-        } else {
-          callbackContext.error("Pinterest couldn't pint it.");
-        }
-      }
-
-      @Override
-      public void onException(Exception e) {
-        super.onException(e);
-        Log.e(TAG, e.getMessage());
-        callbackContext.error(e.getMessage());
-      }
-
-    };
     Log.v(TAG, "execute: action=" + action);
     if (INIT.equals(action)) {
-      Log.v(TAG, "Init: client ID=" + args.getString(0));
-      final String id = args.getString(0);
-      cordova.getThreadPool().execute(new Runnable() {
-                public void run() {
-              PinItButton.setPartnerId(id);
-              PinItButton.setDebugMode(true);
-                    callbackContext.success(); // Thread-safe.
-                }
-            });
+      executeInit(callbackContext, args.getString(0));
       result = true;
     }
     else if (PIN.equals(action)) {
-      sourceUrl = args.getString(0);
-      imageUrl = args.getString(1);
-      description = args.getString(2);
-      Log.v(TAG, "PinIt: source=" + sourceUrl + ", imageUrl: " + 
-        imageUrl + ", description: " + description);
-      cordova.getThreadPool().execute(new Runnable() {
-        public void run() {
-          final PinIt pinIt = new PinIt();
-            pinIt.setUrl(sourceUrl);
-            pinIt.setImageUrl(imageUrl);
-            pinIt.setDescription(description);
-            pinIt.setListener(_listener);
-            pinIt.doPinIt(getApplicationContext());
-          }
-        });
+      executePin(callbackContext, args.getString(0), args.getString(1), args.getString(2));
       result = true;
     } else if (AVAILABLE.equals(action)) {
-      Log.v(TAG, "Checking pinterest availability");
-      callbackContext.error(action + "  is not Supported on Android");
+      executeCheckAvailable(callbackContext);
       result = true;
     }
     return result;
   }
+
+  private void executeInit(CallbackContext callback, final String appId) {
+    Log.v(TAG, "Init: appId=" + appId);
+    callback.success();
+  }
+  private void executePin(CallbackContext callback, final String sourceUrl, final String imageUrl, final String description) {
+    Log.v(TAG, "PinIt: source=" + sourceUrl + ", imageUrl: " +
+            imageUrl + ", description: " + description);
+    Activity activity = cordova.getActivity();
+    String urlRoute = "/pin/create/button/?";
+    if (sourceUrl != null) {
+      urlRoute += "url=" + Uri.encode(sourceUrl) + "&";
+    }
+    if (imageUrl != null) {
+      urlRoute += "media=" + Uri.encode(imageUrl) + "&";
+    }
+    if (description != null) {
+      urlRoute += "description=" + Uri.encode(description);
+    }
+    try {
+      activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("pinterest://www.pinterest.com" + urlRoute)));
+    } catch (Exception e) {
+      activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.pinterest.com" + urlRoute)));
+    }
+    callback.success();
+  }
+
+  private void executeCheckAvailable(CallbackContext callback) {
+    Log.v(TAG, "Checking pinterest availability");
+    callback.error(AVAILABLE + " is not Supported on Android");
+  }
+
 
 }
